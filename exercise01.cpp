66c85f7c88eb1ac19624eb330637ddfba7928047
IT21261282 <99311942+IT21261282@users.noreply.github.com>
2022-02-23 16:05:55 +0000

    Completed

--- exercise01.cpp
+++ exercise01.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
+#include <iomanip>
+
 using namespace std;
-int main() {
+
+int main() 
+{
  float marks[] = {78.4, 90.6, 45.9, 72.2, 54.4};
  char names[][20] = {"Ajith", "Wimal", "Kanthi", "Suranji", "Kushmitha"};
- cout << "No" << "Name" << "Marks" << endl;
- for (int r = 0; r < 5; r++) {
-     cout <<  r+1 
-          <<  names[r]
-          << marks[r] << endl;
- }
+ cout << "No"<<'\t' << "Name"<<'\t' << "Marks" << endl<<endl;
+ for (int r = 0; r < 5; r++) 
+   {
+     cout <<  r+1<<'\t'<<  names[r]<<'\t'<<setw(4)<< setprecision(2)           <<setiosflags(ios::fixed)<< marks[r]<< endl;
+   }
+
+  return 0;
+  
 }
